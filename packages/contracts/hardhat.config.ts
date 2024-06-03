111630a52ebb3cb9f5aab193e955ce495d372ad0
Andrei Riaskov <code@riaskov.com>
2024-06-03 06:53:50 +0300

    fix(contracts): use latest Solidity 0.8.26; contracts are deployed on Polygon and Amoy, fix hardhat config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -6,11 +6,24 @@ import "@nomicfoundation/hardhat-toolbox-viem"
 import '@typechain/hardhat'
 
 const config = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 31337
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY_0],
+      chainId: 137
     },
     polygonAmoy: {
       url: process.env.POLYGON_AMOY_RPC_URL,
@@ -20,6 +33,7 @@ const config = {
   },
   etherscan: {
     apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY,
       polygonAmoy: process.env.POLYGONSCAN_API_KEY
     },
   }
