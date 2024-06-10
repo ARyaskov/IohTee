8031898decd33f0f34167d873c4a482a315f5389
Andrei Riaskov <code@riaskov.com>
2024-06-10 13:36:31 +0300

    fix(examples): rewrite machinomy example

--- packages/examples/src/machinomy.ts
+++ packages/examples/src/machinomy.ts
@@ -1,22 +1,27 @@
-import * as configuration from 'machinomy/lib/configuration'
 import { Machinomy } from '@riaskov/machinomy'
 import express from 'express'
 import bodyParser from 'body-parser'
 import fs from 'fs'
+import { networkByName, NetworkType } from '@riaskov/machinomy-contracts'
+import { getBalance } from 'viem/actions'
+import { createPublicClient, http } from 'viem'
 
 async function main() {
-  let sender: `0x${string}` = '0x0108d76118d97b88aa40167064cb242fa391effa'
-  let receiver: `0x${string}` = '0x3155694d7558eec974cfe35eaa3c2c7bcebb793f'
+  let sender: `0x${string}` = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+  let receiver: `0x${string}` = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
+  const MNEMONIC = String(process.env.ACCOUNT_MNEMONIC).trim()
+  const RPC_URL = String(process.env.RPC_URL).trim()
+  const NETWORK = String(process.env.NETWORK).trim()
+  const chain: any = networkByName(NETWORK) as NetworkType
 
-  let getBalance = async (web3: Web3, account: string) => {
-    return web3.eth.getBalance(account)
-  }
-
-  let provider = configuration.currentProvider()
-  let web3 = new Web3(provider)
-
-  let machinomyHub = new Machinomy(receiver, web3, {
-    databaseUrl: 'nedb://./hub',
+  let machinomyHub = new Machinomy({
+    network: chain,
+    account: receiver,
+    httpRpcUrl: RPC_URL,
+    mnemonic: MNEMONIC,
+    options: {
+      databaseUrl: 'nedb://./hub',
+    },
   })
   let hub = express()
   hub.use(bodyParser.json())
@@ -29,38 +34,49 @@ async function main() {
     },
   )
 
-  let checkBalance = async (
+  async function checkBalance(
     message: string,
-    web3: Web3,
-    sender: string,
+    sender: `0x${string}`,
     cb: Function,
-  ) => {
+  ): Promise<any> {
     console.log('----------')
     console.log(message)
-    let balanceBefore = await getBalance(web3, sender)
-    console.log(
-      'Balance before',
-      web3.fromWei(balanceBefore, 'mwei').toString(),
-    )
-    let result = await cb()
-    let balanceAfter = await getBalance(web3, sender)
-    console.log('Balance after', web3.fromWei(balanceAfter, 'mwei').toString())
-    let diff = balanceBefore.minus(balanceAfter)
-    console.log('Diff', web3.fromWei(diff, 'mwei').toString())
+
+    const balanceBefore = await getBalance(machinomyHub.publicClient(), {
+      address: sender,
+    })
+    console.log('Balance before', balanceBefore / 10n ** 6n)
+
+    const result = await cb()
+
+    const balanceAfter = await getBalance(machinomyHub.publicClient(), {
+      address: sender,
+    })
+    console.log('Balance after', balanceAfter / 10n ** 6n)
+
+    const diff = balanceBefore - balanceAfter
+    console.log('Diff', diff / 10n ** 6n)
+
     return result
   }
 
   let port = 3001
   let server = hub.listen(port, async () => {
-    const price = 1000000
+    const price = 1_000_000n
 
-    let machinomy = new Machinomy(sender, web3, {
-      settlementPeriod: 0,
-      databaseUrl: 'nedb://./client',
+    let machinomy = new Machinomy({
+      network: chain,
+      account: sender,
+      httpRpcUrl: RPC_URL,
+      mnemonic: MNEMONIC,
+      options: {
+        settlementPeriod: 0,
+        databaseUrl: 'nedb://./client',
+      },
     })
 
     let message = 'This is first buy:'
-    let resultFirst = await checkBalance(message, web3, sender, async () => {
+    let resultFirst = await checkBalance(message, sender, async () => {
       return machinomy
         .buy({
           receiver: receiver,
@@ -73,67 +89,71 @@ async function main() {
         })
     })
 
-    message = 'This is second buy:'
-    let resultSecond = await checkBalance(message, web3, sender, async () => {
-      return machinomy
-        .buy({
-          receiver: receiver,
-          price: price,
-          gateway: 'http://localhost:3001/machinomy',
-          meta: 'metaexample',
-        })
-        .catch((e: Error) => {
-          console.log(e)
-        })
-    })
-
-    let channelId = resultSecond.channelId
-    message = 'Deposit:'
-    await checkBalance(message, web3, sender, async () => {
-      await machinomy.deposit(channelId, price)
-    })
-
-    message = 'First close:'
-    await checkBalance(message, web3, sender, async () => {
-      await machinomy.close(channelId)
-    })
-
-    message = 'Second close:'
-    await checkBalance(message, web3, sender, async () => {
-      await machinomy.close(channelId)
-    })
-
-    message = 'Once more buy'
-    let resultThird = await checkBalance(message, web3, sender, async () => {
-      return machinomy
-        .buy({
-          receiver: receiver,
-          price: price,
-          gateway: 'http://localhost:3001/machinomy',
-          meta: 'metaexample',
-        })
-        .catch((e: Error) => {
-          console.log(e)
-        })
-    })
-
-    message = 'Claim by reciver'
-    await checkBalance(message, web3, sender, async () => {
-      await machinomyHub.close(resultThird.channelId)
-    })
-
-    console.log('ChannelId after first buy:', resultFirst.channelId)
-    console.log('ChannelId after second buy:', resultSecond.channelId)
-    console.log('ChannelId after once more buy:', resultThird.channelId)
+    // message = 'This is second buy:'
+    // let resultSecond = await checkBalance(message, sender, async () => {
+    //   return machinomy
+    //     .buy({
+    //       receiver: receiver,
+    //       price: price,
+    //       gateway: 'http://localhost:3001/machinomy',
+    //       meta: 'metaexample',
+    //     })
+    //     .catch((e: Error) => {
+    //       console.log(e)
+    //     })
+    // })
+    //
+    // let channelId = resultSecond.channelId
+    // message = 'Deposit:'
+    // await checkBalance(message, sender, async () => {
+    //   await machinomy.deposit(channelId, price)
+    // })
+    //
+    // message = 'First close:'
+    // await checkBalance(message, sender, async () => {
+    //   await machinomy.close(channelId)
+    // })
+    //
+    // message = 'Second close:'
+    // await checkBalance(message, sender, async () => {
+    //   await machinomy.close(channelId)
+    // })
+    //
+    // message = 'Once more buy'
+    // let resultThird = await checkBalance(message, sender, async () => {
+    //   return machinomy
+    //     .buy({
+    //       receiver: receiver,
+    //       price: price,
+    //       gateway: 'http://localhost:3001/machinomy',
+    //       meta: 'metaexample',
+    //     })
+    //     .catch((e: Error) => {
+    //       console.log(e)
+    //     })
+    // })
+    //
+    // message = 'Claim by receiver'
+    // await checkBalance(message, sender, async () => {
+    //   await machinomyHub.close(resultThird.channelId)
+    // })
+    //
+    // console.log('ChannelId after first buy:', resultFirst.channelId)
+    // console.log('ChannelId after second buy:', resultSecond.channelId)
+    // console.log('ChannelId after once more buy:', resultThird.channelId)
 
     server.close()
     try {
-      fs.unlinkSync('client')
+      if (fs.existsSync('client')) {
+        fs.unlinkSync('client')
+      }
     } catch (error) {
       console.log(error)
     }
     try {
-      fs.unlinkSync('hub')
+      if (fs.existsSync('hub')) {
+        fs.unlinkSync('hub')
+      }
     } catch (error) {
       console.log(error)
     }
