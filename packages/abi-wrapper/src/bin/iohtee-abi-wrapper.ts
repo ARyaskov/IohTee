8aefec8bc9815741e743b0e035e8bc1058376944
Andrei Riaskov <code@riaskov.com>
2024-06-25 10:19:50 +0300

    fix(abi-wrapper): prevent throwing error when no arguments passed

--- packages/abi-wrapper/src/bin/iohtee-abi-wrapper.ts
+++ packages/abi-wrapper/src/bin/iohtee-abi-wrapper.ts
@@ -2,8 +2,9 @@
 
 import yargs from 'yargs'
 import { AbiWrapper } from '../index.js'
+import { hideBin } from 'yargs/helpers'
 
-let args: any = yargs(process.argv.slice(2))
+let args: any = yargs(hideBin(process.argv.slice(2)))
   .option('output', {
     describe: 'Folder for generated files',
     alias: 'o',
@@ -12,6 +13,11 @@ let args: any = yargs(process.argv.slice(2))
     describe: 'Also render minified JS-wrapper',
     alias: 'm',
   }).argv
+
+if (args._.length === 0) {
+  console.log('No arguments provided. Exiting...')
+  process.exit(0)
+}
 
 const pattern = args._[0]
 const outputDir = args['output']
