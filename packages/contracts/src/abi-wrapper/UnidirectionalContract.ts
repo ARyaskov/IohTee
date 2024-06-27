5a378d70416480e2853989a0bbf8fcb20627d5c7
Andrei Riaskov <code@riaskov.com>
2024-06-27 09:04:15 +0300

    fix(contracts): use updated abi-wrapper

--- packages/contracts/src/abi-wrapper/UnidirectionalContract.ts
+++ packages/contracts/src/abi-wrapper/UnidirectionalContract.ts
@@ -1,51 +1,16 @@
 import {
-  Abi,
   AbiEvent,
-  createPublicClient,
-  createWalletClient,
-  getContract,
-  http,
-  PublicClient,
-  WalletClient,
-  GetContractReturnType,
   TransactionReceipt,
   parseEventLogs,
   ParseEventLogsReturnType,
-  extractChain,
 } from 'viem'
-import { mnemonicToAccount } from 'viem/accounts'
-import * as chains from 'viem/chains'
-
-export interface TxOptions {
-  value?: bigint
-}
-
-export type CtorBaseParams = {
-  cachePeriod?: number
-}
-
-export type CtorAccountParamPure = CtorBaseParams & {
-  httpRpcUrl: string
-  networkId: number
-  mnemonic: string
-  hdPath: `m/44'/60'/${string}`
-}
-
-export type CtorAccountParamViem = CtorBaseParams & {
-  publicClientViem: PublicClient
-  walletClientViem: WalletClient
-}
-
-export function isCtorAccountParamPure(
-  params: CtorBaseParams,
-): params is CtorAccountParamPure {
-  return (
-    (params as CtorAccountParamPure).httpRpcUrl !== undefined &&
-    (params as CtorAccountParamPure).mnemonic !== undefined
-  )
-}
-
-export type CtorParams = CtorAccountParamPure | CtorAccountParamViem
+import {
+  BaseContract,
+  TxOptions,
+  CtorParams,
+  isCtorAccountParamPure,
+} from '@riaskov/iohtee-abi-wrapper'
+export { isCtorAccountParamPure, CtorParams, TxOptions }
 
 export interface UnidirectionalEvent {
   eventName: string
@@ -115,13 +80,7 @@ const abi = JSON.parse(
   `"[{\"inputs\":[],\"name\":\"ECDSAInvalidSignature\",\"type\":\"error\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"length\",\"type\":\"uint256\"}],\"name\":\"ECDSAInvalidSignatureLength\",\"type\":\"error\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"s\",\"type\":\"bytes32\"}],\"name\":\"ECDSAInvalidSignatureS\",\"type\":\"error\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"DidClaim\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"deposit\",\"type\":\"uint256\"}],\"name\":\"DidDeposit\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"sender\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"receiver\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"DidOpen\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"DidSettle\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"DidStartSettling\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"addr1\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"addr2\",\"type\":\"address\"}],\"name\":\"debug1\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"payment\",\"type\":\"uint256\"},{\"internalType\":\"address\",\"name\":\"origin\",\"type\":\"address\"},{\"internalType\":\"bytes\",\"name\":\"signature\",\"type\":\"bytes\"}],\"name\":\"canClaim\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"address\",\"name\":\"origin\",\"type\":\"address\"}],\"name\":\"canDeposit\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"canSettle\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"address\",\"name\":\"origin\",\"type\":\"address\"}],\"name\":\"canStartSettling\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"name\":\"channels\",\"outputs\":[{\"internalType\":\"address payable\",\"name\":\"sender\",\"type\":\"address\"},{\"internalType\":\"address payable\",\"name\":\"receiver\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"settlingPeriod\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"settlingUntil\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"payment\",\"type\":\"uint256\"},{\"internalType\":\"bytes\",\"name\":\"signature\",\"type\":\"bytes\"}],\"name\":\"claim\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"deposit\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"isAbsent\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"isOpen\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"isPresent\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"isSettling\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"address\",\"name\":\"receiver\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"settlingPeriod\",\"type\":\"uint256\"}],\"name\":\"open\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"payment\",\"type\":\"uint256\"}],\"name\":\"paymentDigest\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"settle\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"channelId\",\"type\":\"bytes32\"}],\"name\":\"startSettling\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"`,
 )
 
-export class UnidirectionalContract {
-  private readonly _publicClient: PublicClient
-  private readonly _walletClient: WalletClient
-  private readonly _address: `0x${string}`
-  private readonly _contract: GetContractReturnType
-  private readonly _abi: any
-
+export class UnidirectionalContract extends BaseContract {
   /// GETTERS
   async canClaim(
     channelId: `0x${string}`,
@@ -414,70 +373,6 @@ export class UnidirectionalContract {
   }
 
   constructor(deployedContractAddress: `0x${string}`, params: CtorParams) {
-    this._address = deployedContractAddress
-
-    if (isCtorAccountParamPure(params)) {
-      let network = extractChain({
-        chains: Object.values(chains) as any,
-        id: params.networkId,
-      })
-      // @ts-ignore
-      this._publicClient = createPublicClient({
-        batch: {
-          multicall: true,
-        },
-        // @ts-ignore
-        chain: network as any,
-        transport: http(params.httpRpcUrl, {
-          batch: true,
-        }),
-      })
-      this._walletClient = createWalletClient({
-        // @ts-ignore
-        chain: network as any,
-        transport: http(params.httpRpcUrl, {
-          batch: true,
-        }),
-        account: mnemonicToAccount(params.mnemonic, { path: params.hdPath }),
-      })
-    } else {
-      this._publicClient = params.publicClientViem
-      this._walletClient = params.walletClientViem
-    }
-
-    this._abi = abi
-
-    this._contract = getContract({
-      address: this._address,
-      abi: this._abi,
-      client: {
-        public: this._publicClient as never,
-        wallet: this._walletClient as never,
-      },
-    })
-  }
-
-  publicClient(): PublicClient {
-    return this._publicClient
-  }
-
-  walletClient(): WalletClient {
-    return this._walletClient
-  }
-
-  contract(): GetContractReturnType {
-    return this._contract
-  }
-
-  address(): `0x${string}` {
-    return this._address
-  }
-
-  abi(): Abi {
-    return this._abi
-  }
-
-  events(): AbiEvent[] {
-    return this.abi().filter((e) => e.type === 'event') as AbiEvent[]
+    super(deployedContractAddress, params, abi)
   }
 }
